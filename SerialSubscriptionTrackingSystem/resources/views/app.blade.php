55f8f8ef2d4258cd24f083a884e83466d6e12010
Mohasien <mdalingan2@student.apc.edu.ph>
2026-02-19 00:38:44 +0800

    Change the system name

--- SerialSubscriptionTrackingSystem/resources/views/app.blade.php
+++ SerialSubscriptionTrackingSystem/resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title inertia>{{ config('app.name', 'Serial Subscriptions Tracking System') }}</title>
+        <title inertia>{{ config('app.name', 'DOST-STII LAMS') }}</title>
 
         <!-- Favicon -->
         <link rel="icon" type="image/png" href="/images/dost-logo1.png">
